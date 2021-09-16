68e54f8f21e3cc8fd7054d7fe8660a4d290dee39
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-09-16 16:03:00 +0800

    feat: create calculator object in main progra

--- HelloWorld/Person.cs
+++ HelloWorld/Person.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+
 namespace HelloWorld
 {
     public class Person
     {
         public Person()
         {
+        }
+
+        public string FirstName;
+        public string LastName;
+
+        public void Introduction()
+        {
+            Console.WriteLine($"I'm {FirstName} {LastName}");
         }
     }
 }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using HelloWorld.Math;  // to use calculator class
 
 namespace HelloWorld
 {
+    
+    
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(3, 4);
+            Console.WriteLine(result);
+
+
+
+            try
+            {
+                var a = 1000;
+                byte b = Convert.ToByte(a);
+                    
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong");
+
+            }
+
         }
     }
 }
