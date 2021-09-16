47cdc866400b67943bff16f78d079c6a6b7f6127
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-09-16 16:01:51 +0800

    feat: add new class under Math namespace

---
+++ HelloWorld/Math/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld.Math
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
